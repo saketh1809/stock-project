e77cd4804461ce3b44ba967804a8049418aa612c
Saketh Vishwanatham <sakethvishwanatham@gmail.com>
2025-10-30 20:18:08 +0530

    mongodb update

--- check_users.py
+++ check_users.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://mongo:27017/")
+client = MongoClient("mongodb://mongo:27017/stock_app")
 db = client['stock_app']
 users = db['users'].find()
 for user in users:
